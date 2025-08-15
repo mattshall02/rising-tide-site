63cb86dbe3cf70f9163311e506be475c3c41ac14
mattshall02 <77892357+mattshall02@users.noreply.github.com>
2025-08-15 15:30:43 +0000

    Fix build and prepare for Vercel deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,10 @@
-import vercel from '@sveltejs/adapter-vercel';
+// svelte.config.js
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 export default {
   kit: {
-    adapter: vercel()
-  }
-}
+    adapter: adapter()
+  },
+  preprocess: vitePreprocess()
+};
